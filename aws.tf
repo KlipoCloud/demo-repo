01f216361d89d7d3e51c62cbf780a7a8165b39c9
ErsinKirtek <166128528+ErsinKirtek@users.noreply.github.com>
2025-04-07 12:16:45 +0800

    Updated programatically with Klipocloud Agent

--- aws.tf
+++ aws.tf
@@ -84,9 +84,9 @@ resource "aws_ebs_volume" "unassigned_volume" {
 #}
 
 # 3. Orphaned snapshot (not in use)
-resource "aws_ebs_snapshot" "orphaned_snapshot" {
-  volume_id = aws_ebs_volume.unassigned_volume.id
-  tags = {
-    Name = "Orphaned-Snapshot"
-  }
-}
+#resource "aws_ebs_snapshot" "orphaned_snapshot" {
+#  volume_id = aws_ebs_volume.unassigned_volume.id
+#  tags = {
+#    Name = "Orphaned-Snapshot"
+#  }
+#}
