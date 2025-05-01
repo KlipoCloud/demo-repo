3e48e89d9db838efc8e72566d7fe64e7ff84e362
ersinkirteke <ersinkirteke@gmail.com>
2025-05-02 00:22:21 +0300

    test purpose

---
+++ aws2.tf
@@ -0,0 +1,4 @@
+resource "aws_eip_association" "web_eip_assoc" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.unassigned_eip.id
+}
---
+++ aws3.tf
@@ -0,0 +1,22 @@
+# 2. Unassigned EBS volume
+resource "aws_ebs_volume" "unassigned_volume" {
+  availability_zone = "us-east-1a"
+  size             = 10
+  tags = {
+    Name = "Unassigned-EBS"
+  }
+}
+
+resource "aws_volume_attachment" "web_ebs_attach" {
+  device_name = "/dev/xvdf"  # Mounting as a secondary disk
+  volume_id   = aws_ebs_volume.unassigned_volume.id
+  instance_id = aws_instance.web.id
+}
+
+# 3. Orphaned snapshot (not in use)
+resource "aws_ebs_snapshot" "orphaned_snapshot" {
+  volume_id = aws_ebs_volume.unassigned_volume.id
+  tags = {
+    Name = "Orphaned-Snapshot"
+  }
+}
