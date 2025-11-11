c83a85e6c475c90f9c779d8ee8adc7c9dd97fb5c
ErsinKirtek <166128528+ErsinKirtek@users.noreply.github.com>
2025-11-11 18:45:27 +0200

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -46,16 +46,17 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_instance" "web2" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   root_block_device {
-    volume_type = "gp3"
+    volume_type = "gp2"
     volume_size = 20
   }
 }
 
+# 1. Unassigned Elastic IP (EIP)
 resource "aws_eip" "unassigned_eip" {
   tags = {
     Name = "Unassigned-EIP"
@@ -67,20 +68,22 @@ resource "aws_eip_association" "web_eip_assoc" {
   allocation_id = aws_eip.unassigned_eip.id
 }
 
+# 2. Unassigned EBS volume
 resource "aws_ebs_volume" "unassigned_volume" {
   availability_zone = "us-east-1a"
-  size              = 10
+  size             = 10
   tags = {
     Name = "Unassigned-EBS"
   }
 }
 
 resource "aws_volume_attachment" "web_ebs_attach" {
-  device_name = "/dev/xvdf"
+  device_name = "/dev/xvdf"  # Mounting as a secondary disk
   volume_id   = aws_ebs_volume.unassigned_volume.id
   instance_id = aws_instance.web.id
 }
 
+# 3. Orphaned snapshot (not in use)
 resource "aws_ebs_snapshot" "orphaned_snapshot" {
   volume_id = aws_ebs_volume.unassigned_volume.id
   tags = {
