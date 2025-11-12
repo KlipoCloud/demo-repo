694436caf6e156fedc02b21e85a10c9820b68bf6
ErsinKirtek <166128528+ErsinKirtek@users.noreply.github.com>
2025-11-12 15:33:16 +0200

    Updated programmatically with Klipocloud Agent

--- aws.tf
+++ aws.tf
@@ -67,19 +67,19 @@ resource "aws_instance" "web2" {
 #   allocation_id = aws_eip.unassigned_eip.id
 # }
 
-resource "aws_ebs_volume" "unassigned_volume" {
-  availability_zone = "us-east-1a"
-  size             = 10
-  tags = {
-    Name = "Unassigned-EBS"
-  }
-}
+# resource "aws_ebs_volume" "unassigned_volume" {
+#   availability_zone = "us-east-1a"
+#   size             = 10
+#   tags = {
+#     Name = "Unassigned-EBS"
+#   }
+# }
 
-resource "aws_volume_attachment" "web_ebs_attach" {
-  device_name = "/dev/xvdf"  # Mounting as a secondary disk
-  volume_id   = aws_ebs_volume.unassigned_volume.id
-  instance_id = aws_instance.web.id
-}
+# resource "aws_volume_attachment" "web_ebs_attach" {
+#   device_name = "/dev/xvdf"  # Mounting as a secondary disk
+#   volume_id   = aws_ebs_volume.unassigned_volume.id
+#   instance_id = aws_instance.web.id
+# }
 
 # resource "aws_ebs_snapshot" "orphaned_snapshot" {
 #   volume_id = aws_ebs_volume.unassigned_volume.id
