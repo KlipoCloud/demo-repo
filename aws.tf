fa5cd23826c95b2a0a1c31778f2e4cc2528c32a9
ErsinKirtek <166128528+ErsinKirtek@users.noreply.github.com>
2025-12-10 21:35:09 +0100

    Updated programmatically with Klipocloud Agent

--- aws.tf
+++ aws.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_instance" "web2" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
