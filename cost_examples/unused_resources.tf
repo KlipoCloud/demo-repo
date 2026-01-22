cd2d82621ef4a81928470f2dde288bf8391f6548
ErsinKirtek <166128528+ErsinKirtek@users.noreply.github.com>
2026-01-22 21:03:04 +0200

    Updated programmatically with Klipocloud Agent

--- cost_examples/unused_resources.tf
+++ cost_examples/unused_resources.tf
@@ -65,60 +65,60 @@ resource "azurerm_storage_account" "expensive_storage" {
 # }
 
 # SIGNAL: Application Gateway with no backend targets
-resource "azurerm_application_gateway" "unused_appgw" {
-  name                = "appgw-unused"
-  resource_group_name = azurerm_resource_group.unused_resources.name
-  location            = azurerm_resource_group.unused_resources.location
+# resource "azurerm_application_gateway" "unused_appgw" {
+#   name                = "appgw-unused"
+#   resource_group_name = azurerm_resource_group.unused_resources.name
+#   location            = azurerm_resource_group.unused_resources.location
 
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 1 # Minimum for testing
-  }
+#   sku {
+#     name     = "Standard_v2"
+#     tier     = "Standard_v2"
+#     capacity = 1 # Minimum for testing
+#   }
 
-  gateway_ip_configuration {
-    name      = "appgw-ip-config"
-    subnet_id = azurerm_subnet.appgw_subnet.id
-  }
+#   gateway_ip_configuration {
+#     name      = "appgw-ip-config"
+#     subnet_id = azurerm_subnet.appgw_subnet.id
+#   }
 
-  frontend_port {
-    name = "http-port"
-    port = 80
-  }
+#   frontend_port {
+#     name = "http-port"
+#     port = 80
+#   }
 
-  frontend_ip_configuration {
-    name                 = "appgw-frontend-ip"
-    public_ip_address_id = azurerm_public_ip.appgw_ip.id
-  }
+#   frontend_ip_configuration {
+#     name                 = "appgw-frontend-ip"
+#     public_ip_address_id = azurerm_public_ip.appgw_ip.id
+#   }
 
-  backend_address_pool {
-    name = "empty-backend-pool" # SIGNAL: Empty backend pool
-  }
+#   backend_address_pool {
+#     name = "empty-backend-pool" # SIGNAL: Empty backend pool
+#   }
 
-  backend_http_settings {
-    name                  = "http-settings"
-    cookie_based_affinity = "Disabled"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 60
-  }
+#   backend_http_settings {
+#     name                  = "http-settings"
+#     cookie_based_affinity = "Disabled"
+#     port                  = 80
+#     protocol              = "Http"
+#     request_timeout       = 60
+#   }
 
-  http_listener {
-    name                           = "http-listener"
-    frontend_ip_configuration_name = "appgw-frontend-ip"
-    frontend_port_name             = "http-port"
-    protocol                       = "Http"
-  }
+#   http_listener {
+#     name                           = "http-listener"
+#     frontend_ip_configuration_name = "appgw-frontend-ip"
+#     frontend_port_name             = "http-port"
+#     protocol                       = "Http"
+#   }
 
-  request_routing_rule {
-    name                       = "routing-rule"
-    rule_type                  = "Basic"
-    http_listener_name         = "http-listener"
-    backend_address_pool_name  = "empty-backend-pool"
-    backend_http_settings_name = "http-settings"
-    priority                   = 1
-  }
-}
+#   request_routing_rule {
+#     name                       = "routing-rule"
+#     rule_type                  = "Basic"
+#     http_listener_name         = "http-listener"
+#     backend_address_pool_name  = "empty-backend-pool"
+#     backend_http_settings_name = "http-settings"
+#     priority                   = 1
+#   }
+# }
 
 resource "azurerm_virtual_network" "unused_vnet" {
   name                = "vnet-unused"
