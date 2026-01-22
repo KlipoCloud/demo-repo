18acc3fd6997da7ae2aba5edee4555377b02cda6
ErsinKirtek <166128528+ErsinKirtek@users.noreply.github.com>
2026-01-22 20:15:11 +0200

    Updated programmatically with Klipocloud Agent

--- cost_examples/underutilized_vm.tf
+++ cost_examples/underutilized_vm.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_machine" "idle_vm" {
     name              = "idle-os-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
+    managed_disk_type = "Standard_LRS"
   }
 
   storage_image_reference {
@@ -66,5 +66,23 @@ resource "azurerm_network_interface" "idle_nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.idle_subnet.id
     private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_dev_test_schedule" "idle_vm_shutdown" {
+  name                = "idle-vm-auto-shutdown"
+  location            = azurerm_resource_group.underutilized.location
+  resource_group_name = azurerm_resource_group.underutilized.name
+  lab_name            = "idle-vm-lab"
+
+  daily_recurrence {
+    time = "1900"
+  }
+
+  time_zone_id = "Pacific Standard Time"
+  notification_settings {
+    status = "Enabled"
+    email_recipient = "devops@example.com"
+    notification_locale = "en"
   }
 }
