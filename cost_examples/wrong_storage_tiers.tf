6b00034954fbb1efb91da6255ff2ad96489b9344
ErsinKirtek <166128528+ErsinKirtek@users.noreply.github.com>
2026-01-22 22:51:34 +0200

    Updated programmatically with Klipocloud Agent

--- cost_examples/wrong_storage_tiers.tf
+++ cost_examples/wrong_storage_tiers.tf
@@ -10,7 +10,7 @@ resource "azurerm_storage_account" "expensive_archive" {
   resource_group_name      = azurerm_resource_group.wrong_storage.name
   location                 = azurerm_resource_group.wrong_storage.location
   account_tier             = "Standard"
-  account_replication_type = "GRS" # SIGNAL: Geo-redundant for dev data
+  account_replication_type = "LRS" # Updated to use locally redundant storage for cost optimization
 
   blob_properties {
     # SIGNAL: No lifecycle management configured
