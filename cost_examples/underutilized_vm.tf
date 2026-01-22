1c3126b92595bf7e157d87f7b0c1a73266bef943
ErsinKirtek <166128528+ErsinKirtek@users.noreply.github.com>
2026-01-22 23:34:22 +0200

    Updated programmatically with Klipocloud Agent

--- cost_examples/underutilized_vm.tf
+++ cost_examples/underutilized_vm.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_machine" "idle_vm" {
     name              = "idle-os-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
+    managed_disk_type = "Standard_LRS"
   }
 
   storage_image_reference {
@@ -67,4 +67,21 @@ resource "azurerm_network_interface" "idle_nic" {
     subnet_id                     = azurerm_subnet.idle_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
+}
+
+resource "azurerm_dev_test_schedule" "idle_vm_shutdown" {
+  name                = "idle-vm-shutdown-schedule"
+  location            = azurerm_resource_group.underutilized.location
+  resource_group_name = azurerm_resource_group.underutilized.name
+  daily_recurrence {
+    time = "1900"
+  }
+  time_zone_id = "Pacific Standard Time"
+  notification_settings {
+    status = "Enabled"
+    email_recipient = ["devuser@example.com"]
+    webhook_url     = ""
+    minutes_to_notify_before = 15
+  }
+  target_resource_id = azurerm_virtual_machine.idle_vm.id
 }
--- cost_examples/wrong_storage_tiers.tf
+++ cost_examples/wrong_storage_tiers.tf
@@ -63,13 +63,13 @@ resource "azurerm_virtual_machine" "multi_premium_vm" {
     name              = "premium-os"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS" # SIGNAL: Premium OS for dev
+    managed_disk_type = "Standard_LRS" # Updated to use Standard_LRS for cost optimization
   }
 
   # SIGNAL: Multiple premium data disks for development
   storage_data_disk {
     name              = "premium-data-1"
-    managed_disk_type = "Premium_LRS"
+    managed_disk_type = "StandardSSD_LRS" # Updated to use StandardSSD_LRS for cost optimization
     create_option     = "Empty"
     lun               = 0
     disk_size_gb      = "128"
@@ -77,7 +77,7 @@ resource "azurerm_virtual_machine" "multi_premium_vm" {
 
   storage_data_disk {
     name              = "premium-data-2"
-    managed_disk_type = "Premium_LRS"
+    managed_disk_type = "StandardSSD_LRS" # Updated to use StandardSSD_LRS for cost optimization
     create_option     = "Empty"
     lun               = 1
     disk_size_gb      = "256"
@@ -103,6 +103,23 @@ resource "azurerm_virtual_machine" "multi_premium_vm" {
     Environment = "development"
     # SIGNAL: Premium storage for development environment
   }
+}
+
+resource "azurerm_dev_test_schedule" "multi_premium_vm_shutdown" {
+  name                = "auto-shutdown"
+  location            = azurerm_resource_group.wrong_storage.location
+  resource_group_name = azurerm_resource_group.wrong_storage.name
+  lab_name            = "devtestlab"
+  task_type           = "ComputeVmShutdownTask"
+  status              = "Enabled"
+  daily_recurrence {
+    time = "1900"
+  }
+  time_zone_id = "Pacific Standard Time"
+  notification_settings {
+    status = "Disabled"
+  }
+  target_resource_id = azurerm_virtual_machine.multi_premium_vm.id
 }
 
 resource "azurerm_virtual_network" "storage_vnet" {
