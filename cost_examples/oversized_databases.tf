dd4325772477194fd0b574b56bf88905499bb0ec
ErsinKirtek <166128528+ErsinKirtek@users.noreply.github.com>
2026-01-22 20:58:19 +0200

    Updated programmatically with Klipocloud Agent

--- cost_examples/oversized_databases.tf
+++ cost_examples/oversized_databases.tf
@@ -38,7 +38,7 @@ resource "azurerm_postgresql_server" "oversized_postgres" {
   administrator_login          = "psqladmin"
   administrator_login_password = "P@ssw0rd123!"
 
-  sku_name   = "GP_Gen5_8" # SIGNAL: 8 vCores for test environment
+  sku_name   = "GP_Gen5_2" # SIGNAL: 8 vCores for test environment
   version    = "11"
   storage_mb = 102400
 
